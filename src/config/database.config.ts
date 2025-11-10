3ff523b4dd895b350abb9d2c98d77bda5365ecd3
codexuz <examonlineedumo@gmail.com>
2025-11-11 00:14:03 +0500

    "main"

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -3,11 +3,7 @@ import { ConfigService } from '@nestjs/config';
 
 export const getDatabaseConfig = (configService: ConfigService): TypeOrmModuleOptions => ({
   type: 'mysql',
-  host: configService.get<string>('DB_HOST'),
-  port: configService.get<number>('DB_PORT'),
-  username: configService.get<string>('DB_USERNAME'),
-  password: configService.get<string>('DB_PASSWORD'),
-  database: configService.get<string>('DB_DATABASE'),
+  url: configService.get<string>('DATABASE_URL'),
   entities: [__dirname + '/../entities/*.entity{.ts,.js}'],
   synchronize: configService.get<string>('NODE_ENV') === 'development',
   logging: configService.get<string>('NODE_ENV') === 'development',
