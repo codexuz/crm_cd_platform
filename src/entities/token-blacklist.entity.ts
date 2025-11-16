b1bf4f90e85bb2f64fc875ba87fcdd75ee6b4118
codexuz <examonlineedumo@gmail.com>
2025-11-16 11:16:07 +0500

    "main"

--- src/entities/token-blacklist.entity.ts
+++ src/entities/token-blacklist.entity.ts
@@ -12,7 +12,7 @@ export class TokenBlacklist {
   id: string;
 
   @Index()
-  @Column({ type: 'varchar', length: 164 })
+  @Column({ type: 'varchar', length: 512 })
   token: string;
 
   @Column({ nullable: true })
