d64ab61e9d5b0b6d790d91a07819d0de385d9e9b
codexuz <examonlineedumo@gmail.com>
2025-11-17 15:06:02 +0500

    "main"

--- src/main.ts
+++ src/main.ts
@@ -16,7 +16,7 @@ async function bootstrap() {
     new ValidationPipe({
       transform: true,
       whitelist: true,
-      forbidNonWhitelisted: true,
+      forbidNonWhitelisted: false, // Allow but strip extra properties
     }),
   );
 
