b13bf8b616cbbaf195f336d8b6a112a095bd8898
codexuz <examonlineedumo@gmail.com>
2025-11-18 10:52:49 +0500

    "main"

--- src/main.ts
+++ src/main.ts
@@ -25,7 +25,7 @@ async function bootstrap() {
     ? process.env.ALLOWED_ORIGINS.split(',')
     : [
         'http://localhost:3000',
-        'http://localhost:5173',
+        'http://localhost:3001',
         'http://localhost:4200',
         'https://mockmee.uz',
         'https://backend.mockmee.uz',
